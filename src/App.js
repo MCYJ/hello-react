6741970aaf887decb0b8c019bd84a175d1126992
MCYJ <june1012june@gmail.com>
2023-08-06 21:44:15 +0900

    feat: scrollbox component
    - scroll with 'ref' keyword

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import ValidationSample from "./ValidationSample";
 import MyComponent from "./MyComponent";
 import Counter from "./Counter";
 import Say from "./Say";
+import ScrollBox from "./ScrollBox";
 
 class App extends Component {
   // class 형 component에는 render()함수가 꼭 있어야 함
@@ -19,7 +20,7 @@ class App extends Component {
           <h1>리액트가 아닙니다.</h1>
         )}
 */}
-        <ValidationSample></ValidationSample>
+        {/* <ValidationSample></ValidationSample> */}
 
         {/* <MyComponent name="김철수" favoriteNumber={10}>어린이</MyComponent> */}
         {/* <MyComponent name={"react"} favoriteNumber={47}>김철수</MyComponent> */}
@@ -27,7 +28,12 @@ class App extends Component {
         {/* <Say></Say>
         <Counter></Counter> */}
 
-        <EventPractice></EventPractice>
+        {/* <EventPractice></EventPractice> */}
+
+        <ScrollBox ref={(ref) => this.ScrollBox=ref}></ScrollBox>
+        <button onClick={() => this.ScrollBox.scrollToBottom()}>
+          맨 밑으로
+        </button>
       </Fragment>
     );
   }
